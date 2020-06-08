e5b6c9ad89e29e5570edb29cfbd1fc4d4ab19cf0
Terry Cai <gidcai@gmail.com>
2020-06-09 03:35:06 +0800

    SUPPORT  RegExp replace

--- index.js
+++ index.js
@@ -3,27 +3,34 @@
 const through = require("through2");
 const Vinyl = require('vinyl');
 const PluginError = require('plugin-error');
+const rs = require('replacestream');
 
-const anyrepalce = function (callback) {
+const anyrepalce = function (callback, _replacement) {
   var PLUGIN_NAME = 'gulp-anyrepalce';
   return through.obj(function (file, enc, cb) {
     if (file.isNull()) {
       cb(null, file);
       return;
     }
-    if (typeof callback !== 'function') {
-      cb(new PluginError(PLUGIN_NAME, 'Option only support fucntion'));
+    if (file.isStream()) {
+      file.contents = file.contents.pipe(rs(callback, replacement));
+      return cb(null, file);
+    }
+
+    file = new Vinyl(file);
+    if (callback instanceof RegExp) {
+      let replacement = _replacement;
+      if (typeof _replacement === 'function') {
+        replacement = _replacement.bind({ file: file });
+      }
+      file.contents = Buffer.from(String(file.contents).replace(callback, replacement));
+    } else if (typeof callback == 'function') {
+      file.contents = Buffer.from(callback(String(file.contents)));
+    } else {
+      cb(new PluginError(PLUGIN_NAME, 'Option just support fucntion or regexp'));
       return;
     }
-    file = new Vinyl(file);
-    var fileContents = file.contents.toString('utf8');
-
-    const result = callback(fileContents);
-    if (result) {
-      file.contents = Buffer.from(result);
-    }
-    this.push(file);
-    cb();
+    cb(null, file);
   })
 }
 module.exports = anyrepalce;
